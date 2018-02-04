f25c217eb1870f0363f1d1dcd379357598fad8a8
alvaro <a.sevillanodelafuente@gmail.com>
2018-02-04 18:29:21 +0100

    First version

--- reserva_4.sh
+++ reserva_4.sh
@@ -1,5 +1,4 @@
-sudo apt-get --yes --force-yes install firefox=45.0.2+build1-0ubuntu1
-cd /home/ubuntu/Browod
+cd /home/ubuntu/IAmHarder
 sleep 15
 nohup python main.py javi 0 >javi.log 2>&1 &
 sleep 5
--- reserva_5.sh
+++ reserva_5.sh
@@ -1,5 +1,4 @@
-sudo apt-get --yes --force-yes install firefox=45.0.2+build1-0ubuntu1
-cd /home/ubuntu/Browod
+cd /home/ubuntu/IAmHarder
 sleep 15
 nohup python main.py risto 0 >risto.log 2>&1 &
 sleep 5
