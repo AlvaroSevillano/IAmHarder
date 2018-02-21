2ab08969db820e15b52b8a17b01477a0d8bab9b6
alvaro <a.sevillanodelafuente@gmail.com>
2018-02-21 22:19:57 +0100

    Quitar a Ana

--- reserva_2.sh
+++ reserva_2.sh
@@ -2,4 +2,4 @@ cd /home/ubuntu/IAmHarder
 sleep 15
 nohup python main.py leixuri 0 >leixuri.log 2>&1 &
 sleep 5
-nohup python main.py ana 0 >ana.log 2>&1 &
+# nohup python main.py ana 0 >ana.log 2>&1 &
