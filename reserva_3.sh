6e03fa61432c077536a8b68dad5e0139f11ae79f
Álvaro <a.sevillanodelafuente@gmail.com>
2018-07-30 13:00:38 +0200

    Update reserva_3.sh

--- reserva_3.sh
+++ reserva_3.sh
@@ -1,5 +1,5 @@
 cd /home/ubuntu/IAmHarder
 sleep 15
-nohup python main.py ruben 0 >ruben.log 2>&1 &
+# nohup python main.py ruben 0 >ruben.log 2>&1 &
 sleep 5
 nohup python main.py chema 0 >chema.log 2>&1 &
