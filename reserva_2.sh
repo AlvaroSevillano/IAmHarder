f562714ee963c9f98f6d81f5b826f1f76aaa1484
alvaro <a.sevillanodelafuente@gmail.com>
2018-10-21 21:24:46 +0200

    incluir a blas que se había salido al hacer los commits

--- reserva_2.sh
+++ reserva_2.sh
@@ -1,5 +1,5 @@
 cd /home/ubuntu/IAmHarder
 sleep 15
-nohup python main.py leixuri 0 >leixuri.log 2>&1 &
+nohup python main.py blas 0 >blas.log 2>&1 &
 sleep 5
 nohup python main.py jaime 0 >jaime.log 2>&1 &
