903d89be5f6798b62d9bf7e2b5d9a1fe3858feec
alvaro <a.sevillanodelafuente@gmail.com>
2018-11-05 23:24:07 +0100

    incluir a blas que se había salido al hacer los commits

--- reserva_3.sh
+++ reserva_3.sh
@@ -2,4 +2,5 @@ cd /home/ubuntu/IAmHarder
 sleep 15
 nohup python main.py ruben 0 >ruben.log 2>&1 &
 sleep 5
-nohup python main.py chema 0 >chema.log 2>&1 &
+nohup python main.py risto 0 >risto.log 2>&1 &
+#nohup python main.py chema 0 >chema.log 2>&1 &
